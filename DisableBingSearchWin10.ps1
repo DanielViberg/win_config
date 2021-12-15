52e4b27d3b5ab18a0c88ccd3acf307811c389e70
Daniel Viberg <daniel.viberg@intelium.se>
2021-12-15 09:36:10 +0100

    First config

---
+++ DisableBingSearchWin10.ps1
@@ -0,0 +1,4 @@
+if (Test-Path 'HKCU:\Software\Policies\Microsoft\Windows'){
+    New-Item -Path 'HKCU:\Software\Policies\Microsoft\Windows' -Name Explorer -Force
+    New-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Windows\Explorer' -Name DisableSearchBoxSuggestions -Value 1 -PropertyType DWORD -Force
+}
